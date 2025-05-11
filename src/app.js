b7b429c19f6ec414ecf46f3d7268c77b901bc032
Emilio <eljoaquin2005@gmail.com>
2025-05-11 20:37:43 -0300

    creacion de la rama Emilio

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
-//  import express from 'express'
-//  import helmet from 'helmet'
-//  import morgan from 'morgan'
-//  import cors from 'cors'
-//  import cookieParser from 'cookie-parser'
-//  const app = express()
-//  app.use(cors({
-//     origin: "http://localhost:5173",
-//     credentials: true
-//   }));
-//  app.use(morgan('dev'))
-//  app.use(express.json())
-//  app.use(cookieParser())
-//  app.use(helmet())
+ import express from 'express'
+ import helmet from 'helmet'
+ import morgan from 'morgan'
+ import cors from 'cors'
+ import cookieParser from 'cookie-parser'
+ const app = express()
+ app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+  }));
+ app.use(morgan('dev'))
+ app.use(express.json())
+ app.use(cookieParser())
+ app.use(helmet())
 
-//  app.listen(3000, () => {
-//      console.log(`Server corriendo en el puerto: PORT🚀`)
-//  })
+ app.listen(3000, () => {
+     console.log(`Server corriendo en el puerto: PORT🚀`)
+ })
