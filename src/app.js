a274e4627ddfde70f46071ffe9d01928493c8d82
LuciannoFr <luciannotwt@gmail.com>
2025-06-02 16:48:58 -0300

    se mejora la redireccion del gmail y puse mi ip

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(cors({
     origin: [
         "http://127.0.0.1:5500", 
         "http://localhost:5173",
-        "http://192.168.1.4:19000", // Ajusta esta IP
+        "http://192.168.100.116:19000", // Ajusta esta IP
         "http://localhost:19000"
     ],
     credentials: true
