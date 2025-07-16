6db4d77185d760a72584dda9ed60b0760f0575e5
Emiliojoa <emiliojoaquinortizmalich@gmail.com>
2025-07-15 21:34:45 -0300

    .

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(cors({
     origin: [
         "http://127.0.0.1:5500",
         "http://localhost:5173",
-        "http://192.168.0.155:19000", // Ajusta esta IP
+        "http://192.168.0.154:19000", // Ajusta esta IP
         "http://localhost:19000",
     ],
     credentials: true
