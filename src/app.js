4c1d1c51788f0bfed8d1ee99fafa25cced8ac49f
MarceloOrtegaDev <ortegamarcelodavid77@gmail.com>
2025-08-12 14:37:13 -0300

    Se agregó validacion de campos para changePassword

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(cors({
     origin: [
         "http://127.0.0.1:5500",
         "http://localhost:5173",
-        "http://192.168.1.3:19000", // Ajusta esta IP
+        "http://10.254.192.103:19000", // Ajusta esta IP
         "http://192.168.56.1:19000",
         "http://localhost:19000",
     ],
